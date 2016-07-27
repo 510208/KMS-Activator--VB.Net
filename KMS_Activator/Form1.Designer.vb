390dce6c3816cedfb0cfe66d9a9652191cae8bf2
ajc5869 <ajc5869@rit.edu>
2016-07-27 15:24:47 -0400

    Add files via upload

---
+++ KMS_Activator/Form1.Designer.vb
@@ -0,0 +1,297 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.ViewWinActStatBttn = New System.Windows.Forms.Button()
+        Me.ActWinBttn = New System.Windows.Forms.Button()
+        Me.SetWinKMSBttn = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.ViewOffice2013ActStatBttn = New System.Windows.Forms.Button()
+        Me.ActOffice2013Bttn = New System.Windows.Forms.Button()
+        Me.SetOffice2013KMSBttn = New System.Windows.Forms.Button()
+        Me.IPAddressTxtBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.StartKMSServerBttn = New System.Windows.Forms.Button()
+        Me.VMPathBrowseBttn = New System.Windows.Forms.Button()
+        Me.VMPathTxtBox = New System.Windows.Forms.TextBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.ViewOffice2016ActStatBttn = New System.Windows.Forms.Button()
+        Me.ActOffice2016Bttn = New System.Windows.Forms.Button()
+        Me.SetOffice2016KMSBttn = New System.Windows.Forms.Button()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel3.SuspendLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Panel1.Controls.Add(Me.ViewWinActStatBttn)
+        Me.Panel1.Controls.Add(Me.ActWinBttn)
+        Me.Panel1.Controls.Add(Me.SetWinKMSBttn)
+        Me.Panel1.Location = New System.Drawing.Point(11, 163)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(171, 140)
+        Me.Panel1.TabIndex = 0
+        '
+        'ViewWinActStatBttn
+        '
+        Me.ViewWinActStatBttn.Location = New System.Drawing.Point(3, 94)
+        Me.ViewWinActStatBttn.Name = "ViewWinActStatBttn"
+        Me.ViewWinActStatBttn.Size = New System.Drawing.Size(165, 36)
+        Me.ViewWinActStatBttn.TabIndex = 2
+        Me.ViewWinActStatBttn.Text = "View Windows Activation Status"
+        Me.ViewWinActStatBttn.UseVisualStyleBackColor = True
+        '
+        'ActWinBttn
+        '
+        Me.ActWinBttn.Location = New System.Drawing.Point(3, 52)
+        Me.ActWinBttn.Name = "ActWinBttn"
+        Me.ActWinBttn.Size = New System.Drawing.Size(165, 36)
+        Me.ActWinBttn.TabIndex = 1
+        Me.ActWinBttn.Text = "Activate Windows"
+        Me.ActWinBttn.UseVisualStyleBackColor = True
+        '
+        'SetWinKMSBttn
+        '
+        Me.SetWinKMSBttn.Location = New System.Drawing.Point(3, 10)
+        Me.SetWinKMSBttn.Name = "SetWinKMSBttn"
+        Me.SetWinKMSBttn.Size = New System.Drawing.Size(165, 36)
+        Me.SetWinKMSBttn.TabIndex = 0
+        Me.SetWinKMSBttn.Text = "Set Windows KMS Server"
+        Me.SetWinKMSBttn.UseVisualStyleBackColor = True
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Orange
+        Me.Panel2.Controls.Add(Me.ViewOffice2013ActStatBttn)
+        Me.Panel2.Controls.Add(Me.ActOffice2013Bttn)
+        Me.Panel2.Controls.Add(Me.SetOffice2013KMSBttn)
+        Me.Panel2.Location = New System.Drawing.Point(188, 163)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(171, 140)
+        Me.Panel2.TabIndex = 1
+        '
+        'ViewOffice2013ActStatBttn
+        '
+        Me.ViewOffice2013ActStatBttn.Location = New System.Drawing.Point(3, 94)
+        Me.ViewOffice2013ActStatBttn.Name = "ViewOffice2013ActStatBttn"
+        Me.ViewOffice2013ActStatBttn.Size = New System.Drawing.Size(165, 36)
+        Me.ViewOffice2013ActStatBttn.TabIndex = 5
+        Me.ViewOffice2013ActStatBttn.Text = "View Office 2013 Activation Status"
+        Me.ViewOffice2013ActStatBttn.UseVisualStyleBackColor = True
+        '
+        'ActOffice2013Bttn
+        '
+        Me.ActOffice2013Bttn.Location = New System.Drawing.Point(3, 52)
+        Me.ActOffice2013Bttn.Name = "ActOffice2013Bttn"
+        Me.ActOffice2013Bttn.Size = New System.Drawing.Size(165, 36)
+        Me.ActOffice2013Bttn.TabIndex = 4
+        Me.ActOffice2013Bttn.Text = "Activate Office 2013"
+        Me.ActOffice2013Bttn.UseVisualStyleBackColor = True
+        '
+        'SetOffice2013KMSBttn
+        '
+        Me.SetOffice2013KMSBttn.Location = New System.Drawing.Point(3, 10)
+        Me.SetOffice2013KMSBttn.Name = "SetOffice2013KMSBttn"
+        Me.SetOffice2013KMSBttn.Size = New System.Drawing.Size(165, 36)
+        Me.SetOffice2013KMSBttn.TabIndex = 3
+        Me.SetOffice2013KMSBttn.Text = "Set Office 2013 KMS Server"
+        Me.SetOffice2013KMSBttn.UseVisualStyleBackColor = True
+        '
+        'IPAddressTxtBox
+        '
+        Me.IPAddressTxtBox.Location = New System.Drawing.Point(108, 48)
+        Me.IPAddressTxtBox.Name = "IPAddressTxtBox"
+        Me.IPAddressTxtBox.Size = New System.Drawing.Size(309, 20)
+        Me.IPAddressTxtBox.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(94, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "KMS Server IP:"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(191, 76)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(165, 81)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(12, 76)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(167, 81)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 5
+        Me.PictureBox2.TabStop = False
+        '
+        'StartKMSServerBttn
+        '
+        Me.StartKMSServerBttn.Location = New System.Drawing.Point(423, 44)
+        Me.StartKMSServerBttn.Name = "StartKMSServerBttn"
+        Me.StartKMSServerBttn.Size = New System.Drawing.Size(106, 28)
+        Me.StartKMSServerBttn.TabIndex = 6
+        Me.StartKMSServerBttn.Text = "Start KMS Server"
+        Me.StartKMSServerBttn.UseVisualStyleBackColor = True
+        '
+        'VMPathBrowseBttn
+        '
+        Me.VMPathBrowseBttn.Location = New System.Drawing.Point(423, 10)
+        Me.VMPathBrowseBttn.Name = "VMPathBrowseBttn"
+        Me.VMPathBrowseBttn.Size = New System.Drawing.Size(106, 28)
+        Me.VMPathBrowseBttn.TabIndex = 7
+        Me.VMPathBrowseBttn.Text = "Browse"
+        Me.VMPathBrowseBttn.UseVisualStyleBackColor = True
+        '
+        'VMPathTxtBox
+        '
+        Me.VMPathTxtBox.Location = New System.Drawing.Point(15, 15)
+        Me.VMPathTxtBox.Name = "VMPathTxtBox"
+        Me.VMPathTxtBox.ReadOnly = True
+        Me.VMPathTxtBox.Size = New System.Drawing.Size(402, 20)
+        Me.VMPathTxtBox.TabIndex = 8
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.MediumAquamarine
+        Me.Panel3.Controls.Add(Me.ViewOffice2016ActStatBttn)
+        Me.Panel3.Controls.Add(Me.ActOffice2016Bttn)
+        Me.Panel3.Controls.Add(Me.SetOffice2016KMSBttn)
+        Me.Panel3.Location = New System.Drawing.Point(365, 164)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(171, 140)
+        Me.Panel3.TabIndex = 9
+        '
+        'ViewOffice2016ActStatBttn
+        '
+        Me.ViewOffice2016ActStatBttn.Location = New System.Drawing.Point(3, 94)
+        Me.ViewOffice2016ActStatBttn.Name = "ViewOffice2016ActStatBttn"
+        Me.ViewOffice2016ActStatBttn.Size = New System.Drawing.Size(165, 36)
+        Me.ViewOffice2016ActStatBttn.TabIndex = 5
+        Me.ViewOffice2016ActStatBttn.Text = "View Office 2016 Activation Status"
+        Me.ViewOffice2016ActStatBttn.UseVisualStyleBackColor = True
+        '
+        'ActOffice2016Bttn
+        '
+        Me.ActOffice2016Bttn.Location = New System.Drawing.Point(3, 52)
+        Me.ActOffice2016Bttn.Name = "ActOffice2016Bttn"
+        Me.ActOffice2016Bttn.Size = New System.Drawing.Size(165, 36)
+        Me.ActOffice2016Bttn.TabIndex = 4
+        Me.ActOffice2016Bttn.Text = "Activate Office 2016"
+        Me.ActOffice2016Bttn.UseVisualStyleBackColor = True
+        '
+        'SetOffice2016KMSBttn
+        '
+        Me.SetOffice2016KMSBttn.Location = New System.Drawing.Point(3, 10)
+        Me.SetOffice2016KMSBttn.Name = "SetOffice2016KMSBttn"
+        Me.SetOffice2016KMSBttn.Size = New System.Drawing.Size(165, 36)
+        Me.SetOffice2016KMSBttn.TabIndex = 3
+        Me.SetOffice2016KMSBttn.Text = "Set Office 2016 KMS Server"
+        Me.SetOffice2016KMSBttn.UseVisualStyleBackColor = True
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(368, 76)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(165, 81)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 10
+        Me.PictureBox3.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.ClientSize = New System.Drawing.Size(541, 316)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.VMPathTxtBox)
+        Me.Controls.Add(Me.VMPathBrowseBttn)
+        Me.Controls.Add(Me.StartKMSServerBttn)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.IPAddressTxtBox)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "KMS Client Activator v1.5"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel3.ResumeLayout(False)
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents ViewWinActStatBttn As System.Windows.Forms.Button
+    Friend WithEvents ActWinBttn As System.Windows.Forms.Button
+    Friend WithEvents SetWinKMSBttn As System.Windows.Forms.Button
+    Friend WithEvents ViewOffice2013ActStatBttn As System.Windows.Forms.Button
+    Friend WithEvents ActOffice2013Bttn As System.Windows.Forms.Button
+    Friend WithEvents SetOffice2013KMSBttn As System.Windows.Forms.Button
+    Friend WithEvents IPAddressTxtBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents StartKMSServerBttn As System.Windows.Forms.Button
+    Friend WithEvents VMPathBrowseBttn As System.Windows.Forms.Button
+    Friend WithEvents VMPathTxtBox As System.Windows.Forms.TextBox
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents ViewOffice2016ActStatBttn As Button
+    Friend WithEvents ActOffice2016Bttn As Button
+    Friend WithEvents SetOffice2016KMSBttn As Button
+    Friend WithEvents PictureBox3 As PictureBox
+End Class
---
+++ KMS_Activator/Form1.vb
@@ -0,0 +1,101 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles SetWinKMSBttn.Click
+        'Responsible for detecting if IP Address text box is empty. If it is, show error.
+        If String.IsNullOrWhiteSpace(IPAddressTxtBox.Text) Then
+            MessageBox.Show("Please enter an IP address to set as your Windows KMS server", "", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            Shell("cmd.exe /c" & "slmgr.vbs /skms " & IPAddressTxtBox.Text)
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles ActWinBttn.Click
+        'Responsible for activating Windows
+        Shell("cmd.exe /c" & "slmgr.vbs /ato")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles ViewWinActStatBttn.Click
+        'Responsible for displaying detailed activation status
+        Shell("cmd.exe /c" & "slmgr.vbs /dlv")
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles SetOffice2013KMSBttn.Click
+        'Responsible for setting the Office 2013 KMS Server to the value in the IP Address text field
+        If String.IsNullOrWhiteSpace(IPAddressTxtBox.Text) Then
+            MessageBox.Show("Please enter an IP address to set as your Office 2013 KMS server", "", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            Shell("cmd.exe /c" & "cd C:\Program Files (x86)\Microsoft Office\Office15 && cscript ospp.vbs /sethst:" & IPAddressTxtBox.Text & " && pause", vbNormalFocus)
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles ViewOffice2013ActStatBttn.Click
+        'Responsible for displaying Office 2013 detailed activation status
+        Shell("cmd.exe /c" & "cd C:\Program Files (x86)\Microsoft Office\Office15 && cscript ospp.vbs /dstatusall && pause", vbNormalFocus)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles ActOffice2013Bttn.Click
+        'Responsible for activating Office 2013
+        Shell("cmd.exe /c" & "cd C:\Program Files (x86)\Microsoft Office\Office15 && cscript ospp.vbs /act && pause", vbNormalFocus)
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles StartKMSServerBttn.Click
+        'Responsible for starting VM
+        If VMPathTxtBox.Text = "" Then
+            MessageBox.Show("Please enter a path to your KMS virtual machine", "", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            Process.Start(VMPathTxtBox.Text)
+        End If
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles VMPathBrowseBttn.Click
+        'Responsible for opening browse dialog box
+        OpenFileDialog1.ShowDialog()
+        Dim file_path As String = OpenFileDialog1.FileName
+        VMPathTxtBox.Text = file_path
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Form load event to restore text back to IP Address and VM text boxes
+        VMPathTxtBox.Text = My.Settings.File_path_thing
+        If VMPathTxtBox.Text = "" Then
+            VMPathTxtBox.Clear()
+        Else
+            VMPathTxtBox.Text = My.Settings.File_path_thing
+        End If
+
+        IPAddressTxtBox.Text = My.Settings.IP_Address
+        If IPAddressTxtBox.Text = "" Then
+            IPAddressTxtBox.Clear()
+        Else
+            IPAddressTxtBox.Text = My.Settings.IP_Address
+        End If
+    End Sub
+
+
+    Private Sub Form1_FormClosing(sender As Object, e As EventArgs) Handles MyBase.FormClosing
+        'Form close event to save VM path and IP Address Info
+        My.Settings.File_path_thing = VMPathTxtBox.Text
+        My.Settings.IP_Address = IPAddressTxtBox.Text
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles SetOffice2016KMSBttn.Click
+        'Responsible for setting the Office 2016 KMS Server to the value in the IP Address text field
+        If String.IsNullOrWhiteSpace(IPAddressTxtBox.Text) Then
+            MessageBox.Show("Please enter an IP address to set as your Office 2016 KMS server", "", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            Shell("cmd.exe /c" & "cd C:\Program Files (x86)\Microsoft Office\Office16 && cscript ospp.vbs /sethst:" & IPAddressTxtBox.Text & " && pause", vbNormalFocus)
+        End If
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles ActOffice2016Bttn.Click
+        'Responsible for activating Office 2013
+        Shell("cmd.exe /c" & "cd C:\Program Files (x86)\Microsoft Office\Office16 && cscript ospp.vbs /act && pause", vbNormalFocus)
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles ViewOffice2016ActStatBttn.Click
+        'Responsible for displaying Office 2013 detailed activation status
+        Shell("cmd.exe /c" & "cd C:\Program Files (x86)\Microsoft Office\Office16 && cscript ospp.vbs /dstatusall && pause", vbNormalFocus)
+    End Sub
+End Class
